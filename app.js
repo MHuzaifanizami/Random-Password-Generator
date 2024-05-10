6d28c550a31b1f30bb2c4e8c8294c44e3cae980f
M Huzaifa Nizami <mhuzaifanizami@gmail.com>
2024-05-11 01:31:01 +0500

    file updated

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-////////// Making Random Password //////////
-
-var randValue= "!@#$%^&*()_+1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-var password = "";
-
-for (var i = 0; i < 10; i++) {
-    var randNum = Math.floor(Math.random() * randValue.length); 
-    password += randValue[randNum]
-}
-
-var inputBox = document.getElementById("input-box") ;
-// inputBox = password; 
-// console.log(inputBox)
-
-
-
+var inputBox = document.getElementById("inputs");
 
 
 
 ////////// Show & Hidd Password /////////
+
 var passCkeck = document.getElementById("input-icon")
 var showPass = document.getElementById("show-pass")
 var hidePass = document.getElementById("hide-pass")
@@ -44,3 +30,53 @@ inputValue.textContent = inputRange.value;
 inputRange.oninput = function () {
     inputValue.textContent = this.value
 }
+
+////////// Making Random Password //////////
+
+
+// var randValue = "!@#$%^&*()_+1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+// var password = "";
+// var result = "";
+
+
+function check() {
+    var capital = document.getElementById("capital");
+    var small = document.getElementById("small");
+    var number = document.getElementById("number");
+    var symbol = document.getElementById("symbol");
+
+    var capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var smalls = "abcdefghijklmnopqrstuvwxyz";
+    var numbers = "1234567890";
+    var symbols = "!@#$%^&*()_+";
+    var result = "";
+    passw = "";
+
+    var allChar = capitals + smalls + numbers + symbols
+
+    for (var i = 0; i < inputValue.textContent; i++) {
+        passw = Math.floor(Math.random() * allChar.length)
+        result += allChar[passw]
+    }
+    inputBox.value = result;
+    result = "";
+}
+
+
+// for (var i = 0; i < inputValue.textContent ; i++) {
+//     password = Math.floor(Math.random() * randValue.length);
+//     result += randValue[password]
+
+// }
+
+// inputBox.value = result;
+// result = "";
+// }
+
+
+////////// Controlling Check Boxes //////////
+
+
+
+
+
